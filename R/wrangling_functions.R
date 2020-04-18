cd88e39dacce734ccf91782d1ff128c8983b2128
Geoff Miller <gdevonmiller@gmail.com>
2020-04-18 15:41:57 -0500

    Included messages for users that print what files have been read in, the type of data produced (data.frame or tibble), the size of the data (rows x columns) and how many collars were used.

--- R/wrangling_functions.R
+++ R/wrangling_functions.R
@@ -1,7 +1,6 @@
-#' Sort through a directory of Telonics reports and read in a compiled dataset
-#' of most recent broadcasts.
+#' Compile Telonics Reports
 #'
-#' @param  tidy Creates a tibble by default, \code{tidy = FALSE} returns a \code{data.frame}.
+#' @param tidy Creates a tibble by default, \code{tidy = FALSE} returns a \code{data.frame}.
 #' @param path Path of Telonics report direcory.
 #' @examples
 #' wrangle_telonics(path = "~/telonics/reports")
@@ -11,25 +10,42 @@
 wrangle_telonics <- function (path, tidy = TRUE) {
   old <- getwd()
   setwd(path)
-  files <- list.files()
-  files <- subset(files, !grepl("kml", files))
-  files <- subset(files, !grepl("Stat", files))
+  files <- subset(list.files(), grepl("Condensed", list.files()))
   new_files <- c()
   for (i in levels(as.factor(gsub("([0-9]+).*$", "\\1", files)))) {
     collar <- files[gsub("([0-9]+).*$", "\\1", files) == i]
     collar <- collar[as.numeric(gsub("([0-9]+).*$", "\\1", substr(collar, 9, 13))) ==
                        max(as.numeric(gsub("([0-9]+).*$", "\\1", substr(collar, 9, 13))))]
+    message(paste0("Data from file ", collar, " was retrieved."))
     new_files <- c(new_files, collar)
   }
   csv_list <- list()
   for (i in 1:length(new_files)) {
-    csv_list[[i]] <- read.csv(new_files[i], header = TRUE, skip = 23, na.strings = "")
+    csv_list[[i]] <- read.csv(new_files[i], header = TRUE, skip = 23, na.strings = "", stringsAsFactors = FALSE)
     csv_list[[i]]$ctn <- gsub("([0-9]+).*$", "\\1", new_files[i])
   }
   new_file <- do.call(rbind, csv_list)
   if (tidy) {
     new_file <- tibble::tibble(new_file)
+    message(paste0("\nOutput is a tibble of size ",
+                  as.character(dim(new_file)[1]),
+                  " x ",
+                  as.character(dim(new_file)[2]),
+                  " from ",
+                  as.character(length(csv_list)),
+                  " collars."))
+  } else {
+    message(paste0("\nOutput is a data.frame of size ",
+                   as.character(dim(new_file)[1]),
+                   " x ",
+                   as.character(dim(new_file)[2]),
+                   " from ",
+                   as.character(length(csv_list)),
+                   " collars."))
   }
   setwd(old)
   new_file
 }
+
+
+####################### NEXT IDEA: Meta_telonics -> write metadate based on telonics columns after cleaning.
